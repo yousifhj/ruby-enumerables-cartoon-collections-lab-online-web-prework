6c035e7eaabdbf692367ffd2babe2f07f7fe452e
Huda Yousif <hjdoski@gmail.com>
2019-10-01 19:15:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -33,6 +33,12 @@ def long_planeteer_calls(array)
   end 
 end
 
+def find_the_cheese(array)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+    array.find do |type|
+    cheese_types.include?(type)
+  end 
+end
 
 cheese_types = ["cheddar", "gouda", "camembert"]
 snacks = ["crackers", "gouda", "thyme"]
