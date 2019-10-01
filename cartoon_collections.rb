592e7aafce113e8a48db2bc9b4a4d09efda692eb
Huda Yousif <hjdoski@gmail.com>
2019-10-01 19:12:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,11 @@
 dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 
-def roll_call_dwarves[array]
-  # Your code here
+def roll_call_dwarves(array)
+  i = 0 
+  while i < array.length 
+    puts "#{i + 1}. #{array[i]}"
+    i += 1 
+  end 
 end
 
 def summon_captain_planet# code an argument here
