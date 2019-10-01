46b55ebf5c2e42b9d3dfb22a2945ca092b885a2d
Huda Yousif <hjdoski@gmail.com>
2019-10-01 19:14:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,9 +10,17 @@ end
 
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet(array)
+  nu_array = []
+  i = 0 
+  while i < array.length 
+    nu_array << array[i].capitalize + "!"
+    i += 1 
+  end 
+  nu_array
 end
+
 
 def long_planeteer_calls# code an argument here
   # Your code here
